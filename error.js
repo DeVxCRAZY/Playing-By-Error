e1372fe088b11ff5f7c09d39357c3d65ed2a0786
ERROR  ✨ <91991138+DeVxErrorr@users.noreply.github.com>
2021-10-26 19:32:28 +0300

    Create error.js

---
+++ error.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const tpoints = {};
+const prefix = "."
+const vpoints = {};
+const jimp = require("jimp");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+  console.log(
+    `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
+  );
+  let statuses = [
+  
+    `Error`,
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(STREAMING, {      
+    type:"Playing", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+
+client.login("");
